ea246e906146ffc47eebf76e7d1abab2652a692f
thando-10 <thandoxsndlovu@gmail.com>
2023-10-09 03:57:43 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this function will carru all the instructions that will be executed
+ *
+ * Return: this will return the value 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
