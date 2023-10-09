22fa97ca91794b63fdbf251c7561b6e3326a69f3
thando-10 <thandoxsndlovu@gmail.com>
2023-10-09 04:27:24 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - this function will carry the instructions to be executed
+ *
+ * Return: this will return the value 0
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
