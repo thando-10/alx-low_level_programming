a628c7ef5ad07059dd4e3052adb1c2a148bdafee
thando-10 <thandoxsndlovu@gmail.com>
2023-10-10 12:16:59 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - this function will carry all the instructions
+ *
+ * Return: this will return 0 when successful
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
