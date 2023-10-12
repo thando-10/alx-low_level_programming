527b68276772aad96a6630563000c9fc06f630cc
thando-10 <thandoxsndlovu@gmail.com>
2023-10-12 03:36:09 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies 2 integers
+ *@a: char to parameter 1
+ *@b: char to parameter 2
+ *Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
