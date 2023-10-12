ac3ddf3c54009fabb9d33c95b62a5a8a89e61b03
thando-10 <thandoxsndlovu@gmail.com>
2023-10-12 04:45:03 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,10 +2,20 @@
 
 /**
  *print_line - this draws a straight line in the terminal
- *
- *
+ *@n: number if times '_' will be printed
+ *Return: void
  */
 
 void print_line(int n)
 {
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1 ; i <= n ; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
 }
