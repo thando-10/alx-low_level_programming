c919f18c6ac10a22374496ef1c21dd6e0c365dd2
thando-10 <thandoxsndlovu@gmail.com>
2023-10-12 05:03:38 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_diagonal - prints diagonal line in the terminal
+ *@n: number of times '\' should be printed
+ *Return: void
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 0 ; i < n ; i++)
+		{
+			for (j = 0 ; j <= i ; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
