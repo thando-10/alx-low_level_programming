56de1dbfac3c99af063836bd15729196cefd1b8f
thando-10 <thandoxsndlovu@gmail.com>
2023-10-16 03:45:36 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *swap_int - take in two variable integers and swaps them
+ *@change: carries the variable
+ *@a: swaps and stores address of b
+ *@b: swaps and stores address of a
+ *Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int change;
+
+	change = *b;
+	*b = *a;
+	*a = change;
+}
