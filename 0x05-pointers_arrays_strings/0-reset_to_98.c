115b2d19ee1fa131ff63227bcd5cc632b768d1a4
thando-10 <thandoxsndlovu@gmail.com>
2023-10-16 02:56:31 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 -function that takes a pointerr to an int as parameter and updates the value it points to 98
+ *reset_to_98 -takes a pointer to an int as parameter, updates to 98
  *@n: the value to be updated to 98
  *Return: Always 0
  *
