e3097400dfe139b33b7919168882baa10abecf79
thando-10 <thandoxsndlovu@gmail.com>
2023-10-27 14:36:16 -0700

    ddd

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - multiplies two numbers
+ *@argc: aruguments count
+ *@argv: arguments
+ *
+ *Return: always 0
+ */
+
+int main(int argc, char **argv)
+{
+	int x, y;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	printf("%d\n", x = y);
+
+	return (0);
+}
