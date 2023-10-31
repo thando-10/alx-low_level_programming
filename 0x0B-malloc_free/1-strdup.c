235527d3f32d256ef084eb8b09aa82b9f15f8c51
thando-10 <thandoxsndlovu@gmail.com>
2023-10-31 01:36:05 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - return a pointer to a newly alllocated
+ *spcae in memory also duplicate the string it holds
+ *to the new place
+ *
+ *
+ *@str: initial input string
+ *
+ *Return: pointer to the memory allocated
+ */
+
+char *_strdup(char *str)
+{
+	size_t i, j;
+	char *strdout;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	strdout = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (strdout == NULL)
+	{
+		return (NULL);
+	}
+
+	for (j = 0; j <= i; j++)
+	{
+		strdout[j] = str[j];
+	}
+	return (strdout);
+}
