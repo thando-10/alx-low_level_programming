c7a9cf33d0d1fefa981f15d9285155d9a850385d
thando-10 <thandoxsndlovu@gmail.com>
2023-11-06 01:39:49 -0800

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - prints the name of the file of the program
+ *was complied from, followed by a new line.
+ *
+ *Return: always 0 on success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
