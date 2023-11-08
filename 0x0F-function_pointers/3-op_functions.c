a1004c6fdacfde421ec14cc44cf745f861d2b42a
thando-10 <thandoxsndlovu@gmail.com>
2023-11-08 12:15:36 -0800

    task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,72 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+*op_add - returns the sum of two numbers
+*@a: the first number
+*@b: the second number
+*
+*Return: the sum of a and b
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+*op_sub - returns the difference of two numbers
+*@a: the first number
+*@b: the second number
+*
+*Return: the difference of a and b
+*/
+
+int op_sub (int a, int b)
+{
+	return (a - b);
+}
+
+/**
+*op_mul - returns product of two numbers
+*@a: the first number
+*@b: the second number
+*
+*Return: the product of a and b
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+*op_div - returns division of two numbers
+*@a: the first number
+*@b: the second number
+*
+*Return: the quotient of a and b
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+*op_mod - returns the reminder of the division of two numbers
+*@a: the first number
+*@b: the second number
+*
+*Return: the reminder of the division of a and b
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
