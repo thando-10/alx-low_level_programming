44973c689a8d3239f61f4d4b68abd9ca9c645c52
thando-10 <thandoxsndlovu@gmail.com>
2023-11-16 03:09:48 -0800

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *list_len - show the number of elements of a list
+ *@h: linked list
+ *
+ *Return: the number of elements of a list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
