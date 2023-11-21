924ea9e5df2317f6d4036660134df68fca9b74e3
thando-10 <thandoxsndlovu@gmail.com>
2023-11-21 05:42:10 -0800

    task 33

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,7 +8,7 @@
  *Return: pointer to the new node, or NULL if fails
  */
 
-listint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
