1b06d069660b15609f67523793cb85e91836cd86
thando-10 <thandoxsndlovu@gmail.com>
2023-11-30 05:46:38 -0800

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *create_file - create a file and copy content
+ *@filename: the filename to be created
+ *@text_content: the content to be copied into file
+ *
+ *Return: 1 if succesful or -1 if failed
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+	close(fd);
+
+	return (1);
+}
